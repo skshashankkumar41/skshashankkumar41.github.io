7f41a7d01e414531338ccfbdca558a907435e74e
Shashank Prasad <skshashankkumar41@gmail.com>
2020-06-22 09:47:02 +0530

    added mouse over event listener

--- multifaceted_digit_recog/static/js/draw.js
+++ multifaceted_digit_recog/static/js/draw.js
@@ -1,32 +1,11 @@
 
-/*window.onload = function(){
-    var canvas = document.getElementById('canvas');
-    var ctx = canvas.getContext('2d');
-    var painting = document.getElementById('paint');
-    var paint_style = getComputedStyle(painting);
-    var mouse = {x: 0, y: 0};
+window.onload = function(){
+
     canvas.addEventListener('mousemove', function(e) {
         document.getElementById("resetButton").scrollIntoView();
-        mouse.x = e.pageX - this.offsetLeft;
-        mouse.y = e.pageY - this.offsetTop;
     }, false);
-    ctx.lineWidth = 25;
-    ctx.lineJoin = 'round';
-    ctx.lineCap = 'round';
-    canvas.addEventListener('mousedown', function(e) {
-        ctx.beginPath();
-        ctx.moveTo(mouse.x, mouse.y);
-        canvas.addEventListener('mousemove', onPaint, false);
-    }, false);
-    canvas.addEventListener('mouseup', function() {
-    canvas.removeEventListener('mousemove', onPaint, false);
-    }, false);
-    var onPaint = function() {
-    ctx.lineTo(mouse.x, mouse.y);
-    ctx.stroke();
-    };
  
-} */
+} 
 
 
 makePostCall = function (url, data) { 
