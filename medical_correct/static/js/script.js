6e8f8263021d5502b14161fb9b683cf44e2f589d
Shashank Prasad <skshashankkumar41@gmail.com>
2020-07-08 16:07:41 +0530

    added typewriter

--- medical_correct/static/js/script.js
+++ medical_correct/static/js/script.js
@@ -10,10 +10,48 @@ makePostCall = function (url, data) {
     });
 }
 
+function typeWriter(selector_target, text_list, placeholder = false, i = 0, text_list_i=0, delay_ms=80) {
+    if (!i) {
+        if (placeholder) {
+            document.querySelector(selector_target).placeholder = "";
+        }
+        else {
+            document.querySelector(selector_target).innerHTML = "";
+        }
+    }
+    txt = text_list[text_list_i];
+    if (i < txt.length) {
+        if (placeholder) {
+            document.querySelector(selector_target).placeholder += txt.charAt(i);
+        }
+        else {
+            document.querySelector(selector_target).innerHTML += txt.charAt(i);
+        }
+        i++;
+        setTimeout(typeWriter, delay_ms, selector_target, text_list, placeholder, i, text_list_i);
+    }
+    else {
+        text_list_i++;
+        if (typeof text_list[text_list_i] === "undefined")  {
+            // set "return;" for disabled infinite loop 
+            setTimeout(typeWriter, (delay_ms*5), selector_target, text_list, placeholder);
+        }
+        else {
+            i = 0;
+            setTimeout(typeWriter, (delay_ms*3), selector_target, text_list, placeholder, i, text_list_i);
+        }
+    }
+}
+
 window.onload = function(){
     document.getElementById("textMedical").onclick = function() {
         document.getElementById("resetButton").scrollIntoView();
     };
+
+    timeout_var = null;
+    text_list = ["I need gaencolgist","Opthlmogst","Curuna virus","I have chst pain"];
+
+    return_value = typeWriter("#textMedical", text_list, true);
 }
 
 
--- sentiment_analyzer/static/js/script.js
+++ sentiment_analyzer/static/js/script.js
@@ -13,17 +13,54 @@ makePostCall = function (url, data) {
     });
 }
 
+function typeWriter(selector_target, text_list, placeholder = false, i = 0, text_list_i=0, delay_ms=80) {
+    if (!i) {
+        if (placeholder) {
+            document.querySelector(selector_target).placeholder = "";
+        }
+        else {
+            document.querySelector(selector_target).innerHTML = "";
+        }
+    }
+    txt = text_list[text_list_i];
+    if (i < txt.length) {
+        if (placeholder) {
+            document.querySelector(selector_target).placeholder += txt.charAt(i);
+        }
+        else {
+            document.querySelector(selector_target).innerHTML += txt.charAt(i);
+        }
+        i++;
+        setTimeout(typeWriter, delay_ms, selector_target, text_list, placeholder, i, text_list_i);
+    }
+    else {
+        text_list_i++;
+        if (typeof text_list[text_list_i] === "undefined")  {
+            // set "return;" for disabled infinite loop 
+            setTimeout(typeWriter, (delay_ms*5), selector_target, text_list, placeholder);
+        }
+        else {
+            i = 0;
+            setTimeout(typeWriter, (delay_ms*3), selector_target, text_list, placeholder, i, text_list_i);
+        }
+    }
+}
+
 
 window.onload = function(){
     document.getElementById("textMedical").onclick = function() {
         document.getElementById("resetButton").scrollIntoView();
     };
-}
 
+    timeout_var = null;
+    text_list = ["Sometimes i think god is very kind to me","I want to kill you", "What a wonderful world","I want to die"];
+
+    return_value = typeWriter("#textMedical", text_list, true);
+}
 
 function getCorrect(){
     console.time("start");
-    alert("Server Issue!!! Process will Take Around 10 Seconds")
+    alert("Server Issue!!! Process will Take Around 15-20 Seconds Please Wait :)")
     var text = document.getElementById("textMedical").value;
     console.log("GOT THE DATA")
     console.log(text)
